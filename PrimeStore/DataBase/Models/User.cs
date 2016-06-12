dcb32a9626156cc47234ef7bafb8b7716bc1f72c
TheBestWorldProgramer <vity199600@mail.ru>
2016-06-12 18:15:31 +0300

    wpf

--- PrimeStore/DataBase/Models/User.cs
+++ PrimeStore/DataBase/Models/User.cs
@@ -11,12 +11,88 @@ namespace DataBase.Models
 {
     class User
     {
-        public virtual int UserId { get; set; }
-        public virtual string Login { get; set; }
-        public virtual string FirstName { get; set; }
-        public virtual string LastName { get; set; }
-        public virtual string Email { get; set; }
-        public virtual Role role { get; set; }
-        public virtual ICollection<Product> product { get; set; }
+        private int userId;
+        private string login;
+        private string firstName;
+        private string lastName;
+        private string email;
+        private Role role;
+        private ICollection<Product> products;
+
+        public virtual int UserId
+        {
+            get
+            {
+                return userId;
+            }
+        }
+
+        public virtual string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+            }
+        }
+
+        public virtual string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public virtual string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public virtual string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public virtual Role Role
+        {
+            get
+            {
+                return role;
+            }
+        }
+
+        public virtual ICollection<Product> Products
+        {
+            get
+            {
+                return products?? new List<Product>();
+            }
+        }
     }
 }
